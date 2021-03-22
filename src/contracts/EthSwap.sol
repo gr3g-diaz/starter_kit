b967f1c06c23c83bac487763c3e9692b54e1ed21
gregorydiaz <gr3gdiaz@gmail.com>
2021-03-21 17:07:58 -0700

    More unit tests
    Added more unit tests.
    Added solidity event to buyTokens

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,36 @@
 pragma solidity ^0.5.0;
 
+import "./Token.sol";
+
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint public rate = 100;
+
+    event TokenPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    // payable allows us to send ether
+    function buyTokens() public payable {
+        // Redemption rate = # of tokens they receive for 1 ether.
+        // Amount of Ethereum * Redemption rate.
+        // Unsigned integer is a POSITIVE int with no decimal places.
+        uint tokenAmount = msg.value * rate;
+        // msg is a global variable in solidity and sender is address. 
+        token.transfer(msg.sender, tokenAmount);
+
+
+        //Emit an event
+        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+
+    }
+
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -16,12 +16,12 @@ function tokens(n) {
     return web3.utils.toWei(n, 'ether');
 }
 
-contract('EthSwap', (accounts) => {
+contract('EthSwap', ([deployer, investor]) => {
     let token, ethSwap
 
     before(async ()=> {
         token = await Token.new()
-        ethSwap = await EthSwap.new()
+        ethSwap = await EthSwap.new(token.address)
         await token.transfer(ethSwap.address, tokens('1000000'))
     })
 
@@ -42,6 +42,37 @@ contract('EthSwap', (accounts) => {
     it('contract has tokens', async () => {
         let balance = await token.balanceOf(ethSwap.address);
         assert.equal(balance.toString(),tokens('1000000'))
+    });
+
+    describe('buyTokens()', async () => {
+
+        let result 
+        before(async () => {
+            //Purchase tokens before each example
+            result = await ethSwap.buyTokens({from: investor, value: web3.utils.toWei('1', 'ether')});
+        });
+
+
+        it('Allows user to instantly purchase tokens for a fixed price.', async () => {
+            // Check investor balance after purchase
+            let investorBalance = await token.balanceOf(investor)
+            assert.equal(investorBalance.toString(), tokens('100'))
+
+            // Check ethSwap balance after purchase.
+            let ethSwapBalance
+            ethSwapBalance = await token.balanceOf(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), tokens('999900'))
+
+            ethSwapBalance = await web3.eth.getBalance(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), web3.utils.toWei('1', 'ether'))
+
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount.toString(), tokens('100').toString())
+            assert.equal(event.rate.toString(), '100')
+
+        });
     });
 
 })
